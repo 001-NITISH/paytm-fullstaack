d67eb5b48fa841bb1166e13df3ae2ec025b21ac3
001-NITISH <kumarneeku620@gmail.com>
2024-05-06 16:07:21 +0530

    Frontent completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
-import ReactDOM from "react-dom/client";
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+
+import {Signup} from "./pages/Signup";
+import {Dashboard} from "./pages/Dashboard";
+import {Signin} from "./pages/Signin";
+import {SendMoney} from "./pages/SendMoney";
 
 function App() {
 
@@ -7,11 +11,11 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path = "/signup" element={<Signup/>}/>
-          <Route path = "/signin" element={<Signin/>}/>
-          <Route path = "/dashboard" element={<Dashboard/>}/>
-          <Route path = "/send" element={<SendMoney/>}/>
-        </Routes>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/signin" element={<Signin/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/send" element={<SendMoney/>}/>
+        </Routes>        
       </BrowserRouter>
     </>
   )
